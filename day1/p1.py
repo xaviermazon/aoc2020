691e54694ba5ba305d212d42752292fa9acb1268
Xavier Mazon <xavi.mazon.bcn@gmail.com>
2021-07-17 10:44:29 +0200

    Dia 1 del AOC 2020

---
+++ day1/p1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def busca2020():
+    for i in range(0,len(Lista_de_numeros)):
+        for j in range(0,len(Lista_de_numeros)):
+            if (Lista_de_numeros[i] + Lista_de_numeros[j]magit
+Lista_de_numeros = []
+with open('problem.in', 'r') as file:
+    Lista_de_numeros = [int(linea) for linea in file]
+
+print(str(busca2020()))
---
+++ day1/p2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def busca2020():
+    for i in range(0,len(Lista_de_numeros)):
+        for j in range(0,len(Lista_de_numeros)):
+            for k in range(0,len(Lista_de_numeros)):
+                if (Lista_de_numeros[i] + Lista_de_numeros[j] + Lista_de_numeros[k]) == 2020:
+                    return Lista_de_numeros[i] * Lista_de_numeros[j] * Lista_de_numeros[k]
+
+Lista_de_numeros = []
+with open('problem.in', 'r') as file:
+    Lista_de_numeros = [int(linea) for linea in file]
+
+print(str(busca2020()))
