ad96e1fc355a30019652e880096b170f754bb7d1
Xavier Mazon <xavi.mazon.bcn@gmail.com>
2021-08-01 22:15:26 +0200

    Arreglando el p1.py

--- day1/p1.py
+++ day1/p1.py
@@ -3,7 +3,9 @@
 def busca2020():
     for i in range(0,len(Lista_de_numeros)):
         for j in range(0,len(Lista_de_numeros)):
-            if (Lista_de_numeros[i] + Lista_de_numeros[j]magit
+            if (Lista_de_numeros[i] + Lista_de_numeros[j] == 2020):
+                return Lista_de_numeros[i] * Lista_de_numeros[j]
+                
 Lista_de_numeros = []
 with open('problem.in', 'r') as file:
     Lista_de_numeros = [int(linea) for linea in file]
